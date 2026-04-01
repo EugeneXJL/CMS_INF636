15262087291422a7538d6656c04fdc234e856ba2
Eugene Xu <eugene.jlx@gmail.com>
2026-04-02 01:22:47 +1000

    020426-1110 Fix unused import in Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
-import { useNavigate, Link, useLocation } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axiosInstance from '../axiosConfig';
 
 const Login = () => {
